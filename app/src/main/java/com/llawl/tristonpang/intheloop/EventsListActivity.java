9d02f639810579281aebf9747f57340dfc017bea
NELVINPC\HP <nelvintan@gmail.com>
2018-06-17 11:45:24 +0800

    Added organisers page.

--- app/src/main/java/com/llawl/tristonpang/intheloop/EventsListActivity.java
+++ app/src/main/java/com/llawl/tristonpang/intheloop/EventsListActivity.java
@@ -9,5 +9,5 @@ public class EventsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_list);
-}
+    }
 }
